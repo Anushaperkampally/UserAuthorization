721ffde75eaf0b36b48994c87cd6076688fc4383
Anushaperkampally <122984856+Anushaperkampally@users.noreply.github.com>
2023-02-08 08:44:24 +0530

    code changes

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,32 +13,52 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./login.component.scss']
 })
 
+export class LoginComponent implements OnInit {
 
-export class LoginComponent  {
+public loginform!: FormGroup
 
-
-public loginform:FormGroup = new FormGroup({
-email:new FormControl(),
-password:new FormControl(),
-
-})
-constructor(private loginservice:LoginService, private router:Router) {}
+constructor(private formbuilder:FormBuilder,private http:HttpClient,private router:Router){}
 
   login(){
-    this.loginservice.getlogin(this.loginform.value).subscribe(
-      (value:LoginResponce)=>{
-        alert("login sucessfully")
-        localStorage.setItem("token",value.token);
-        this.router.navigateByUrl("/UsermainComponent")
-      },
-      (error:any)=>{
-        console.log("error")
-        alert("login failed invalid credaintels")
+    this.http.get<any>("http://localhost:3000/superadmin" && "http://localhost:3000/adminlogin").subscribe(res=>{
+      const user =res.find((a:any)=>{
+        return a.email === this.loginform.value.email && a.password === this.loginform.value.password
+      });
+      if(user){
+        alert("loginsuccesfull");
+        this.loginform.reset()
+        this.router.navigate(["UsermainComponent"])
       }
-    )
+      else{
+        alert("user not found")
+        this.router.navigate(["login"])
+      }
+      (error:any)=>{
+        alert("something went wrong")
+      }
+    })
   }
 
+  close(){
+           this.router.navigate(["login"])
+  }
+
+  get Email(): FormControl {
+     return this.loginform.get("email")as FormControl;
+   }
+
+   get Password(): FormControl {
+     return this.loginform.get("password")as FormControl;
+   } 
+
+  ngOnInit(): void {
+    this.loginform=this.formbuilder.group({
+      email:new FormControl("",[Validators.required,Validators.email]),
+      password:new FormControl("",[Validators.required,Validators.maxLength(15),Validators.minLength(6)])
 
 
+    })
+  }    
 
-}
+
+ }
